7213193c79107b36f34e212ef44700b301639be4
vitekkor <viktor.korotkikh@gmail.com>
2020-04-22 21:49:06 +0300

    some changes 22/04/2020 21:41

--- core/src/test/java/test/LevelTest.kt
+++ core/src/test/java/test/LevelTest.kt
@@ -2,9 +2,8 @@ package test
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.utils.JsonReader
-import com.vitekkor.polytech.objects.Level
-import org.junit.jupiter.api.Test
 
+import org.junit.jupiter.api.Test
 internal class LevelTest {
 
     @Test
@@ -12,6 +11,5 @@ internal class LevelTest {
         val json = Gdx.files.internal("levels/levels.json").file().readText()
         val jsonReader = JsonReader()
         val levels = jsonReader.parse(json)
-        val lvl = Level(levels)
     }
 }
